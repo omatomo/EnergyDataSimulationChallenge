e36398b324932fdde8c89acf92f220533e8ce40a
omatomo <ajadj4@gmail.com>
2018-01-10 22:39:06 +0900

    flash機能を追加

--- challenge3/webapp/omatomo/app/models/energy.rb
+++ challenge3/webapp/omatomo/app/models/energy.rb
@@ -1,6 +1,16 @@
 require 'csv'
 require 'kconv'
 class Energy < ApplicationRecord
+  belongs_to :house
+  
+  validates :label,             presence: true
+  validates :house_id,          presence: true
+	validates :house,             presence: true
+	validates :year,              presence: true
+  validates :month,             presence: true
+	validates :temperature,       presence: true
+	validates :daylight,          presence: true
+  validates :energy_production, presence: true
 
   def Energy.import_csv(csv_file)
     csv_text = csv_file.read
